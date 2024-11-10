171c182d8c197be4f8d91280882a11b16754faee
matheuz-rodrigues <matheusrodriguesfilho.91@gmail.com>
2024-11-10 06:02:42 -0300

    Sistema de boas vindas atualizado. Não está marcando o usuario novo

--- utils/welcome.py
+++ utils/welcome.py
@@ -1,7 +1,17 @@
 from  bot_instance import bot
 import discord
 
+
 @bot.event
-async def on_member_join(ctx):
+async def on_member_join(member):
     channel_id = 1293004888716087298
-    await bot.get_channel(channel_id).send(f"{ctx.mention} acabou de ser commitado na {ctx.guild}! :space_invader:")
+    channel = bot.get_channel(channel_id)
+
+
+    embed = discord.Embed(
+        description=f"{member.mention} acabou de ser commitado na {member.guild}! :space_invader:",
+        color=discord.Color.blue()
+    )
+    embed.set_image(url="https://media.tenor.com/Qr56AbZspAoAAAAM/celebrate-futurama.gif")
+
+    await channel.send(embed=embed)
