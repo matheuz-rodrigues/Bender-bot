7f5a0a3a83db9806475d6d085701273a576dfdb1
MarcosOki <marcosokita181@gmail.com>
2024-11-10 22:53:36 -0300

    :bug: fix: arrumei o bug de não mencionar o member no evento de bem vindo

--- utils/welcome.py
+++ utils/welcome.py
@@ -7,11 +7,10 @@ async def on_member_join(member):
     channel_id = 1293004888716087298
     channel = bot.get_channel(channel_id)
 
-
     embed = discord.Embed(
-        description=f"{member.mention} acabou de ser commitado na {member.guild}! :space_invader:",
+        description=f" Acabou de ser commitado na {member.guild}! :space_invader:",
         color=discord.Color.blue()
     )
     embed.set_image(url="https://media.tenor.com/Qr56AbZspAoAAAAM/celebrate-futurama.gif")
-
-    await channel.send(embed=embed)
+    embed.set_author(name=member, icon_url=member.avatar.url)
+    await channel.send(f"{member.mention}",embed=embed)
