9a8f3a0f733493a6ee35916ed3ba4e5a87f193ee
matheuz-rodrigues <matheusrodriguesfilho.91@gmail.com>
2024-11-11 12:23:38 -0300

    Coinflip transicionado para slash

--- minigames/coinflip.py
+++ minigames/coinflip.py
@@ -1,4 +1,6 @@
 from bot_instance import bot
+import discord
+from discord import app_commands
 from random import choice
 from time import sleep
 
@@ -17,3 +19,25 @@ async def coinflip(ctx, escolha: str = None):
             await ctx.send(f"Resultado: **{moeda} Voce perdeu!**")
     else:
         await ctx.send("Opção Inválida, tente novamente.")
+
+@bot.tree.command(name='coinflip', description="Jogue Cara ou Coroa comigo")
+@app_commands.describe(escolha="Escolha entre cara ou coroa")
+async def coinflip(interaction: discord.Interaction, escolha: str = None):
+    if escolha is None:
+        await interaction.response.send_message("Use **/coinflip** seguido de sua escolha (cara ou coroa).", ephemeral=True)
+        return
+
+    escolha = escolha.lower()
+    if escolha in ["cara", "coroa"]:
+        await interaction.response.send_message("A moeda foi lançada...")
+        sleep(2)
+
+        moeda = choice(["cara", "coroa"])
+
+        if moeda == escolha:
+            await interaction.followup.send(f"Resultado: **{moeda}**. Você venceu!")
+        else:
+            await interaction.followup.send(f"Resultado: **{moeda}**. Você perdeu!")
+    else:
+        await interaction.response.send_message("Opção inválida. Tente novamente com 'cara' ou 'coroa'.", ephemeral=True)
+
--- minigames/ppt.py
+++ minigames/ppt.py
@@ -26,3 +26,24 @@ async def ppt(interaction: discord.Interaction, escolha: str = None):
         await interaction.response.send_message(f"Eu escolhi {minhaEscolha.capitalize()} e você {escolha.capitalize()}. Parabéns pela vitória!")
     else:
         await interaction.response.send_message(f"Eu escolhi {minhaEscolha.capitalize()} e você {escolha.capitalize()}. Tenta na próxima, otário.")
+
+@bot.command()
+async def ppt(ctx, escolha: str = None):
+
+    opcoes = ["pedra", "papel", "tesoura"]
+    minhaEscolha = opcoes[randint(0, 2)]
+
+    if escolha is None:
+        await ctx.send("Digite **.ppt pedra, papel ou tesoura** para jogar comigo.")
+        return
+    elif escolha.lower() not in opcoes:
+        await ctx.send("Opção Inválida, tente novamente.")
+        return
+    elif escolha.lower() == minhaEscolha:
+        await ctx.send("Bom jogo, infelizmente empatamos!")
+        #Caos que o bot perde
+    elif escolha.lower() == "pedra" and minhaEscolha == "tesoura" \
+    or escolha.lower() == "papel" and minhaEscolha == "pedra"\
+    or escolha.lower() == "tesoura" and minhaEscolha == "papel":
+        await ctx.send(f"Eu escolhi {minhaEscolha.capitalize()} e você {escolha.capitalize()}, parabéns pela Vitória.")
+    else: await ctx.send(F"Eu escolhi {minhaEscolha.capitalize()} e você {escolha.capitalize()}, tenta na próxima otário.")
