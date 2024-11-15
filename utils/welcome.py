01bbf05685c344a1bf1b96846be1c5e798096baa
MarcosOki <marcosokita181@gmail.com>
2024-11-15 20:13:55 -0300

    Sistema de Auto Role finalizado

--- utils/welcome.py
+++ utils/welcome.py
@@ -1,11 +1,17 @@
 from  bot_instance import bot
 import discord
-
+from discord.utils import get
 
 @bot.event
 async def on_member_join(member):
     channel_id = 1293004888716087298
     channel = bot.get_channel(channel_id)
+    role1 = get(member.guild.roles, name="Pequeno Gafanhoto")
+    role2 = get(member.guild.roles, name="GarticMod")
+    user_id = member.id
+    user = await member.guild.fetch_member(user_id)
+    await user.add_roles(role1)
+    await user.add_roles(role2)
 
     embed = discord.Embed(
         description=f" Acabou de ser commitado na {member.guild}! :space_invader:",
