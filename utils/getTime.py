71106f2f967bc95d4348953a43fe2a8a768ec98c
MarcosOki <marcosokita181@gmail.com>
2024-12-01 19:20:37 -0300

    Adicionado sistema pra caso a pessoa ainda não tenha entrado em call

--- utils/getTime.py
+++ utils/getTime.py
@@ -16,7 +16,13 @@ async def tempo(ctx):
         minutes = int((data["time"] % 3600) / 60)
         seconds = int(data["time"] % 60)
         embed = discord.Embed(
-                description=f"**{ctx.author}** disperdiçou um total de {hours}:{minutes}:{seconds} no servidor.",
+                description=f"**{ctx.author}** disperdiçou um total de {hours} horas {minutes} minutos e {seconds} segundos no servidor.",
+                color=discord.Color.blue()
+            )
+        await ctx.send(embed=embed)
+    else:
+        embed = discord.Embed(
+                description=f"**{ctx.author}** Você ainda não particiou de nenhuma call nesse servidor. Parabéns!",
                 color=discord.Color.blue()
             )
         await ctx.send(embed=embed)
